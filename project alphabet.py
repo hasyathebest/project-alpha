234584a290e972396aebf7af9e18db0b67c6f99a
hasyathebest <mrhasyashah@gmail.com>
2025-05-11 13:16:41 +1000

    project alphabet.py

--- project alphabet.py
+++ project alphabet.py
@@ -1,14 +1,5 @@
 alpha=input ("enter anything and this machine will tell you if it is a alphabet or not")
-if alpha==str:
-    h=3
-elif alpha==float:
-    h=4
-elif alpha==int:
-    h=5
-
-if h==3:
-    print("that is a string number not a decimal muber or even a normal number")
-elif h==4:
-    print("that is a decimal number")
-elif h==5:
-    print("that is a normal number")
+if alpha.isalpha():
+    print("this is an alphabet")
+else:
+    print("this is not an alphabet")
